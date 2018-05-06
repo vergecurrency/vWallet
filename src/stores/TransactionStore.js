8e13adfe1413a0edcd59f1bd4fade5a61729c6bf
marvin.piekarek <marvin.piekarek@signavio.com>
2018-05-06 08:33:51 +0200

    Fix bug where client was undefined

--- src/stores/TransactionStore.js
+++ src/stores/TransactionStore.js
@@ -2,36 +2,36 @@ import { observable, action, computed } from 'mobx'
 import { Client } from 'verge-node-typescript'
 
 class TransactionStore {
-  @observable transactions = []
+	@observable transactions = []
 
-  @action
-  addTransaction = transaction => {
-    this.transactions.push(transaction)
-  }
+	@action
+	addTransaction = transaction => {
+		this.transactions.push(transaction)
+	}
 
-  @action
-  addTransactions = transactions => {
-    this.transactions = [...this.transactions, ...transactions]
-  }
+	@action
+	addTransactions = transactions => {
+		this.transactions = [...this.transactions, ...transactions]
+	}
 
-  @computed
-  get getTransactionCount() {
-    return this.transaction.lenght
-  }
+	@computed
+	get getTransactionCount() {
+		return this.transaction.lenght
+	}
 }
 
 const store = new TransactionStore()
 
 const client = new Client({ user: 'kyon', pass: 'lolcat' })
-  .getTransactionList()
-  .then(transactions => {
-    store.addTransactions(transactions)
-  })
+
+client.getTransactionList().then(transactions => {
+	store.addTransactions(transactions)
+})
 
 setInterval(() => {
-  client.getTransactionList().then(transactions => {
-    store.addTransactions(transactions)
-  })
+	client.getTransactionList().then(transactions => {
+		store.addTransactions(transactions)
+	})
 }, 5000)
 
 export default store
