8b3eb409822b19197b207b5751fe75875c59c363
Swen van Zanten <swen@troublefree.nl>
2018-07-06 00:32:43 +0200

    Reformatted the modal html

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -17,11 +17,7 @@ export default ({ open, toggle, title, children, style }: ModalProps) => (
     centered={true}
     style={{ width: '547px', height: '688px', ...style }}
   >
-    <ModalHeader toggle={toggle}>
-      {title}
-    </ModalHeader>
-    <ModalBody>
-      {children}
-    </ModalBody>
+    <ModalHeader toggle={toggle}>{title}</ModalHeader>
+    <ModalBody>{children}</ModalBody>
   </Modal>
 )
