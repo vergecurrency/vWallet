ca9a1f214223f450697c1f5a45e22baaedbcfed3
Swen van Zanten <swen@troublefree.nl>
2018-07-19 23:20:44 +0200

    Added mock data to the dev-json Added all mock data to the MockClient Changed the readme

--- src/utils/mockups/MockClient.ts
+++ src/utils/mockups/MockClient.ts
@@ -2,74 +2,99 @@ import { Transaction } from 'verge-node-typescript/dist/Transaction'
 import { Client } from 'verge-node-typescript'
 import { Peer } from 'verge-node-typescript/dist/Peer'
 import { WalletInfo } from 'verge-node-typescript/dist/WalletInfo'
-import MockWalletInfo from './MockWalletInfo'
-const faker = require('faker')
 
-function generateTransaction() {
-  const time = Math.trunc(faker.date.recent(12).getTime() / 1000)
-  return {
-    txid: faker.random.alphaNumeric(64),
-    vout: faker.random.alphaNumeric(64),
-    account: 'my account',
-    address: faker.finance.bitcoinAddress(),
-    category: faker.random.boolean() ? 'receive' : 'send',
-    amount: 123.456789 * faker.random.number(100, 1),
-    confirmations: faker.random.number(),
-    blockhash: faker.random.alphaNumeric(64),
-    blockindex: faker.random.alphaNumeric(64),
-    blocktime: time,
-    time: time + 10000,
-    timereceived: time + 5000,
-  }
+const { mockData: mockData } = require('../../dev-config.json')
+
+interface Info extends WalletInfo {
+  unlocked?: boolean
+  isunlocked: boolean
 }
 
-const { mockClient: mockClient } = require('../../dev-config.json')
-const transactions = Array(mockClient.transactions).fill(null).map(() => generateTransaction())
-
 class MockClient extends Client {
+  info: Info
+  unlocked: boolean
+  isunlocked: boolean
+
+  constructor(credentials) {
+    super(credentials)
+
+    const info = JSON.parse(localStorage.info)
+
+    this.unlocked = info.unlocked
+    this.isunlocked = info.isunlocked
+  }
+
   unlockWallet(passphrase: any, timeout?: number): Promise<boolean> {
     return new Promise((respond, reject) => {
-      return respond(true)
+      if (mockData.passphrase === passphrase) {
+        this.info.unlocked = this.unlocked = true
+        this.info.isunlocked = this.isunlocked = true
+        return respond(this.info.unlocked)
+      }
+
+      if (this.info.unlocked) {
+        return reject(['already unlocked'])
+      }
+
+      reject([])
     })
   }
+
   getBalance(account?: string): Promise<number> {
     return new Promise<number>((respond, reject) => {
-      return respond(20202)
+      return respond(mockData.balance)
     })
   }
+
   getInfo(): Promise<WalletInfo> {
     return new Promise((respond, reject) => {
-      const walletInfo = new MockWalletInfo()
-      return respond(walletInfo)
+      this.info = mockData.walletInfo
+      this.info.unlocked = this.unlocked
+      this.info.isunlocked = this.isunlocked
+      return respond(this.info)
     })
   }
+
   getPeerInfo(): Promise<Array<Peer>> {
     return new Promise((respond, reject) => {
-      return respond([])
+      return respond(mockData.peers)
     })
   }
+
   getTransactionList(
     count?: number,
     from?: number,
     account?: string,
   ): Promise<Transaction[]> {
     return new Promise((respond, reject) => {
-      return respond(transactions)
+      return respond(mockData.transactions)
     })
   }
+
   sendToAddress(address: string, amount: number): Promise<Client> {
     return new Promise((respond, reject) => {
       return respond(this)
     })
   }
+
   walletLock(): Promise<any> {
     return new Promise((respond, reject) => {
-      return respond(false)
+      this.info.unlocked = this.unlocked = false
+      this.info.isunlocked = this.isunlocked = false
+      return respond(this.info.unlocked)
     })
   }
+
   getNewAddress(): Promise<string> {
     return new Promise((respond, reject) => {
-      return respond(faker.finance.bitcoinAddress())
+      let text = ''
+      const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
+
+      for (let i = 0; i < 34; i++) {
+        text += possible.charAt(Math.floor(Math.random() * possible.length))
+      }
+
+      return respond(text)
     })
   }
 }
