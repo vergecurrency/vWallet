f8fb99d4ff5bf250bb8fc6a8542a96c68d23d0c8
marpme <cryzar95@googlemail.com>
2018-09-09 14:59:31 +0200

    ledger integration

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,18 @@
+const { defaults } = require('jest-config')
+
 module.exports = {
   roots: ['<rootDir>/test'],
   transform: {
     '^.+\\.tsx?$': 'ts-jest',
   },
   testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$',
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  moduleFileExtensions: [
+    ...defaults.moduleFileExtensions,
+    'ts',
+    'tsx',
+    'js',
+    'jsx',
+    'json',
+    'node',
+  ],
 }
