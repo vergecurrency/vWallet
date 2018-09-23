62a417e3465d43569a9c0e6503af1651300319f2
marpme <marvin.piekarek5@gmail.com>
2018-09-23 14:05:56 +0200

    Adding features to wallet * check if wallet exists * get Path * locked/unlocked check

--- src/crypto/Wallet.ts
+++ src/crypto/Wallet.ts
@@ -35,9 +35,40 @@ export class VergeLightClient {
     this.walletPath = path.join(getUserDataPath(), settings.WALLET_PATH_NAME)
   }
 
+  /**
+   * Checks if the wallet that is being used
+   * is ready for full usage!
+   *
+   * (best check for start up a wallet)
+   */
+  public isWalletReady(): boolean {
+    return this.client.isComplete()
+  }
+
+  /**
+   * Checks if the currently used wallet is encrypted
+   *
+   * Kinda like a locked/unlocked check
+   */
+  public isWalletLocked(): boolean {
+    return this.client.isPrivKeyEncrypted()
+  }
+
+  public isWalletAlreadyExistent(): boolean {
+    return fs.existsSync(this.walletPath)
+  }
+
+  /**
+   * Creates OR overrides a wallet
+   * @param passphrase
+   */
   public createNewWallet(passphrase: string): Promise<CreatedWalletData> {
     invariant(!!passphrase, 'No Empty passphrase allowed')
-
+    // check that we aren't mindlessly overwriting wallets
+    invariant(
+      this.isWalletAlreadyExistent(),
+      'Existing wallet can`t be overriden!',
+    )
     // creates a new wallet with all data (privKey,Mnemonic ...)
     this.client.seedFromRandomWithMnemonic({
       passphrase,
@@ -65,6 +96,29 @@ export class VergeLightClient {
         },
       )
     })
+  }
+
+  /**
+   * Restores a wallet with a given mnmoic
+   * Basic restoring of keys and additional information
+   *
+   * No transactions or Balances or used Addresses
+   * Those have to be synced afterwards manually.
+   */
+  public restoreWalletFromMnemoic(
+    mnemonic: string,
+    passphrase: string,
+  ): Promise<CreatedWalletData> {
+    invariant(!!mnemonic, `Mnemonic is required and can't be emtpy`)
+    invariant(!!passphrase, `Passphrase is required and can't be emtpy`)
+    // check that we aren't mindlessly overwriting wallets
+    invariant(
+      this.isWalletAlreadyExistent(),
+      'Existing wallet can`t be overriden!',
+    )
+
+    // TODO: TO BE IMPLEMENTED
+    return Promise.resolve({} as CreatedWalletData)
   }
 }
 
